61ad4c4c53cb9e68962566a33d07677af48128e1
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2023-12-04 13:38:49 -0300

    module 5 finished

--- Dynamic Web Apps/views/index.view.php
+++ Dynamic Web Apps/views/index.view.php
@@ -4,7 +4,8 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
-        <p class="mx-auto text-md">Welcome to my page built with PHP & TailwindCSS</p>
+        <p class="mx-auto text-md">Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to my page built with PHP & TailwindCSS.</p>
+
     </div>
 </main>
 </div>
